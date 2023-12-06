fb1430bd3dc1cef9418e8054aaa3c647f34ce8a4
bunionl <lttrujillo10@gmail.com>
2023-12-06 10:45:25 -0600

    done

--- SHA256.py
+++ SHA256.py
@@ -6,11 +6,13 @@ file = open("commonPasswords.txt", "r")
 common_passwords = file.readlines()
 # Hash of the password to be attacked
 hashed_password = input("Enter the SHA256 hash: ")
+#print(hashed_password)
 # Runs popular passwords and encodes them in SHA256 to compare to the given hash
 i = 0
 answr = False
 while i < len(common_passwords):
-    possible_password = common_passwords[i]
+    possible_password = common_passwords[i].rstrip()
+    #print(possible_password)
     hashed_possible_password = hashlib.sha256(bytes(possible_password, 'utf-8')).hexdigest()
     if hashed_possible_password == hashed_password:
             print(f"Password found: {possible_password}")
